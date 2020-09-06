5bbf0a11b2045efb48b1f223f480870fa928cc15
Serge Pekarsky <serge.pekarsky@gmail.com>
2020-09-06 21:54:58 +0200

    Integration test w/mockMvc

--- src/test/java/com/test/filerecords/web/RecordsControllerTest.java
+++ src/test/java/com/test/filerecords/web/RecordsControllerTest.java
@@ -1,25 +1,49 @@
 package com.test.filerecords.web;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.FileInputStream;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 class RecordsControllerTest {
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    public void shouldUpload1RecordFileAndSuccessfullyGetRecordAnd404OnWrongId() throws Exception {
+        FileInputStream fis = new FileInputStream("src/test/resources/testfiles/testFile_success_one_record.txt");
+        MockMultipartFile upload = new MockMultipartFile("file", fis);
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.multipart("/")
+                        .file(upload);
+        mockMvc.perform(builder).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.get("/111")).andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/123456")).andExpect(status().isNotFound());
     }
 
     @Test
-    void upload() {
+    public void shouldThrowExceptionOnWrongFileAndNoRecordsSaved() throws Exception {
+        FileInputStream fis = new FileInputStream("src/test/resources/testfiles/testFile_error_empty_line.txt");
+        MockMultipartFile upload = new MockMultipartFile("file", fis);
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.multipart("/")
+                        .file(upload);
+        mockMvc.perform(builder).andExpect(status().isBadRequest()).andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.get("/1000")).andExpect(status().isNotFound());
     }
 
-    @Test
-    void getById() {
-    }
-
-    @Test
-    void getByPeriod() {
-    }
 }
